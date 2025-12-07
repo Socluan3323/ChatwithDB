0e69ccc97cb36f091e6ea619dcbab3180c2e203d
NhatNM <123147485+NguyenNhat4@users.noreply.github.com>
2025-12-07 20:58:58 +0700

    a

--- call_llm.py
+++ call_llm.py
@@ -15,31 +15,3 @@ def call_llm(prompt):
     return response.text
 
 
-def build_prompt(text):
-    return f"""
-    
-    phân loại các sản phẩm trong List sau: 
-    {text}
-    này thành một danh sách tương ứng , thuộc type_id như sau:
-    - 1.VEGETABLES
-    - 2.FRUITS
-    - 3.GRAINS_LEGUMES
-    - 4.DAIRY_EGGS
-    - 5.MEAT_SEAFOOD
-    format:
-    ```yaml
-    prouduct_id: type_id
-    ```
-    example :
-    ```yaml
-    1:2
-    2:1
-    3:5
-    4:5
-    5:5
-    6:2
-    7:1
-    ```
-    
-    trả về chinh xac câu trúc YML như trên:
-    """
---
+++ ingest_data.py
@@ -0,0 +1,121 @@
+from typing import Dict, List
+
+from crud import *
+from clean_data import convert_list_to_dict,convert_string_to_dict,convert_string_to_list
+from call_llm import call_llm
+from yaml import safe_load
+
+
+init_schema()
+
+productype_id_to_name = {
+    "FP" : "thuc pham",
+    "DR" : "do uong",
+    "HG" : "do gia dung",
+    "EL" : "thiet bi dien tu",
+    "CS" : "my pham"
+}
+productype_name_to_id = {
+    "thuc pham" : "FP",
+    "do uong" : "DR",
+    "do gia dung" : "HG",
+    "thiet bi dien tu" : "EL",
+    "my pham" : "CS"
+}
+
+prompt_classify = """ 
+
+Bạn là trợ lý hỗ trợ phân loại cặp product_id: product_name thành các cặp:
+- product_type_id: product_type_name
+{list_product_type_name}
+
+Example:
+input:
+- hk: banh mi
+- gh: iphone
+
+output với format sau:
+
+'''yaml
+hk: FP
+gh: EL
+''' 
+hiện tại input là :
+{list_pair_product_id_name}
+
+hãy trả về output như ví dụ trên.
+
+
+
+"""
+
+
+def format_dictionary_to_string(dictionary: Dict[str,str]) -> str:
+    product_id = dictionary.get("product_id",None)
+    name = dictionary.get("name", None)
+    return f"{product_id}: {name}"
+    """
+    {'product_id': 'EL035',
+    'name': 'Day HDMI 3m',
+    'price': '120000',
+    'quantity': '110'}
+    
+    output :
+    
+      "-EL035: Day HDMI 3m"
+    """
+    
+    
+def create_format_list_product_id_name(t: List[Dict[str,str]])->str:
+    """
+    INPUT:
+    [{'product_id': 'FP001',
+    'name': 'Gao thom ST25',
+    'price': '18000',
+    'quantity': '120'},
+    ...
+    ]
+        
+    
+    
+    output:
+    "-FP001: Gao thom ST25
+    - ...
+    - ... 
+    - ..."
+
+    
+    """
+    return "\n".join(format_dictionary_to_string(item) for item in t)
+      
+  
+    
+
+for type_id,type_name in productype_id_to_name.items():
+    a = ProductType(id= type_id,name= type_name)
+    insert_one_productType(a)
+    
+    
+with open("newdata.txt","r") as f:
+    data_rows = f.readlines()
+    
+    
+clean_data = [convert_string_to_dict(x,",") for x in data_rows if convert_string_to_dict(x,",")]
+
+list_product_type_name = "\n".join(f"-{a[0]}: {a[1]}" for a in productype_id_to_name.items())
+complete_prompt = prompt_classify.format(list_pair_product_id_name = create_format_list_product_id_name(clean_data), list_product_type_name = list_product_type_name)
+
+
+result = call_llm(complete_prompt)
+product_id_to_type_id = safe_load(result.split("```yaml")[1].split("```")[0])
+
+print(product_id_to_type_id)
+lists_product = []
+for item in clean_data:
+    lists_product.append(Product(id = item.get("product_id"), 
+        name = item.get("name"),
+        price = item.get("price"),
+        quantity = item.get("quantity"),
+        type_id = product_id_to_type_id.get(item.get("product_id"))))
+    
+insert_list_products(lists_product)
