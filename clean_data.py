c91794db90a0d37b9431ba99830c318e467ef214
Đình Quân Võ <vodinhquan2714@gmail.com>
2025-12-07 14:16:55 +0100

    a

--- clean_data.py
+++ clean_data.py
@@ -4,6 +4,23 @@ import os
 from config import FILE_PATH
 
 
+
+
+
+def is_valid_row(clean_row: List[str]) -> bool:
+    try:
+        if len(clean_row) != 4:
+            raise IndexError("Mảng phải đủ 4 phần tử")
+        if not clean_row[2].isnumeric():
+            raise ValueError("price phải là số")
+        if not clean_row[3].isnumeric():
+            raise ValueError("quantity phải là số")
+        return True
+    except Exception as e:
+        print(e)
+        return False
+
+
  
 def convert_string_to_list(one_line: str , seperate_by: str = None) ->List[str]: 
     """
@@ -16,8 +33,16 @@ def convert_string_to_list(one_line: str , seperate_by: str = None) ->List[str]:
 
     
     """
+    split_row = []
+    if seperate_by is None:
+        split_row = [x.strip() for x in one_line.split()]
+    else:
+        split_row = [x.strip() for x in one_line.split(seperate_by)]
     
+    if not is_valid_row(split_row):
+        return None
     
+    return split_row
     # clean_row = []
     # name = ""
     # for element in one_line:
@@ -29,7 +54,6 @@ def convert_string_to_list(one_line: str , seperate_by: str = None) ->List[str]:
     # name = name.strip()
     # clean_row.insert(1, name)
     # return clean_row
-    return [x.strip() for x in one_line.split(seperate_by)]
 
 def convert_list_to_dict (rows: List[List[str]]) -> Dict[str,str]:
     """
@@ -58,6 +82,29 @@ def convert_list_to_dict (rows: List[List[str]]) -> Dict[str,str]:
                 }
     except Exception as e:
         print(e,rows)
+        return None
+
+
+def convert_string_to_dict(one_line: str, seperate_by: str = None) -> Dict[str,str]:
+    """
+    input
+    one_line = ['FP001','Gao thom ST25','18000','120'], seperate_by = ','
+
+    output
+    {
+        'product_id': 'FP001'
+        'name': 'Gao thom ST25'
+        'price':'18000'
+        'quantity': '120'
+    }
+    
+    """
+    clean_list = convert_string_to_list(one_line, seperate_by)
+    if clean_list is not None:
+        return convert_list_to_dict(clean_list)
+    
+    return None
+
 
 if __name__ == '__main__':
     # convert_string_to_list(FILE_PATH)
